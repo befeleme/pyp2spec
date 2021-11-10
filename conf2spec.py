3eebab49378ec15e0ea43034af5d61cb04a91c43
Karolina Surma <ksurma@redhat.com>
2021-11-10 13:41:10 +0100

    Move ConfigFile to conf2spec

--- conf2spec.py
+++ conf2spec.py
@@ -1,11 +1,47 @@
 from pathlib import Path
 
 import click
+import tomli
+
 from jinja2 import Template
 
-from config import ConfigFile
 
 TEMPLATE_PATH = Path(__file__).resolve().parent / 'template.spec'
+
+
+class ConfigFile:
+    """Load configuration file and return its validated values."""
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.contents = self.load_configuration
+
+    @property
+    def load_configuration(self):
+        """Return loaded TOML configuration file contents."""
+
+        with open(self.filename, "rb") as configuration_file:
+            loaded_contents = tomli.load(configuration_file)
+
+        return loaded_contents
+
+    def get_string(self, key):
+        """Return a value for given key. Validate the value is a string.
+        Raise TypeError otherwise."""
+
+        return self._get_value(key, str)
+
+    def get_list(self, key):
+        """Return a value for given key. Validate the value is a list.
+        Raise TypeError otherwise."""
+
+        return self._get_value(key, list)
+
+    def _get_value(self, key, val_type):
+        val = self.contents.get(key, val_type())
+        if not isinstance(val, val_type):
+            raise TypeError(f"{val} must be a {val_type}")
+        return val
 
 
 def generate_extra_build_requires(config):
