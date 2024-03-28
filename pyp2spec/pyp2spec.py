3582ffc16dcd26277a63ca76984e086e7b0677f9
Martin Juhl <m@rtinjuhl.dk>
2024-03-28 11:59:50 +0100

    Added options to pyp2spec

--- pyp2spec/pyp2spec.py
+++ pyp2spec/pyp2spec.py
@@ -13,12 +13,17 @@ from pyp2spec.utils import Pyp2specError
     "--spec-output", "-o",
     help="Provide custom output where spec file will be saved",
 )
+@click.option(
+    "--python-version",
+    "-p",
+    help="Specify specific python version to build for, e.g 3.11",
+)
 def main(**options):
     click.secho("Generating configuration file", fg="cyan")
     try:
         config_file = create_config(options)
         click.secho("Generating spec file", fg="cyan")
-        create_spec_file(config_file, options["spec_output"])
+        create_spec_file(config_file, options["spec_output"],options["python_version"])
     except (Pyp2specError, NotImplementedError) as exc:
         click.secho(f"Fatal exception occurred: {exc}", fg="red")
         sys.exit(1)
